63ece90b9e7ce5d686950a28ce16461a83f2d963
AlwanY <ebisukuru@gmail.com>
2020-11-22 03:22:00 +0700

    Change home page from /homePage to /

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -3,7 +3,7 @@ import PostController from '../../controllers/PostController'
 
 const router = express.Router()
 
+router.get('/', PostController.getHomePageView)
 router.get('/game', PostController.getGameView)
-router.get('/homePage', PostController.getHomePageView)
 
 export default router
